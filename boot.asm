8af34b21ef38b97ed874a7a28a345e9942e8cdf6
malloc <dimeo.dario.h@gmail.com>
2026-08-09 14:23:05 +0200

    Add es to initialised segment registers. Add extra sector reading via CHS addressing

--- boot.asm
+++ boot.asm
@@ -5,13 +5,29 @@ BITS 16
     jmp 0x0000:start
 start:
 
-    ; set ss and ds to 0x0000
+    ; set ss, ds, and es to 0x0000
     xor ax, ax
     mov ss, ax ; (sp yet to be set)
     mov ds, ax
+    mov es, ax
 
     ; finish to set the stack (below the master boot record)
     mov sp, 0x7C00
+
+    ; set up registers to query int 0x13
+    mov ah, 0x02    ; read
+    mov al, 0x01    ; number ofsectors
+    mov ch, 0x00    ; 0-7 cylinder bits
+    mov cl, 0x02    ; 8-9 cylinder bits - sector
+    mov dh, 0x00    ; head
+                    ; dl is assumed to be set by the BIOS
+                    ; es was already set to 0x0000
+    mov bx, 0x7E00  ; destination offset
+
+    int 0x13
+    jc  safe_hlt    ; halt on failure
+
+    ; ...
 
 safe_hlt:
     hlt
@@ -19,3 +35,5 @@ safe_hlt:
 
 TIMES 510 - ($ - $$) DB 0
 DW 0xAA55
+
+TIMES 512 db 0xDC   ; test value
